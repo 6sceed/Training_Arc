54d044c55176b27cab2f01aab0729e99a2a5f631
sceed <6ckgarcia@gmail.com>
2025-08-15 19:24:22 +0800

    cpp in/out

---
+++ C++/Day1sec1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    string one = "hello world!";
+    string two = "test";
+    string three = "ts boring";
+    string y = "";
+
+    cout << one << "hi" << endl;
+    cin >> y;
+    cout << "what u up to?" << endl;
+    cin >> y;
+    cout << "alr gl" << endl;
+
+    return 0;
+}
